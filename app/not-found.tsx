03e3b327a9310c583bba7e669b81042a8cd2731e
NjohPrince <jufredprince@gmail.com>
2025-07-21 08:53:27 +0100

    feat(page): adjusted the global not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,9 +1,11 @@
 import Link from "next/link";
 
+import classes from "./not-found.module.css";
+
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
+    <div className={`${classes.not__found} flex center col gap-12`}>
+      <h2>Oops... Not Found</h2>
       <p>Could not find requested resource</p>
       <Link href="/">Return Home</Link>
     </div>
