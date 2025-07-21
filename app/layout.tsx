f6671f5534c3bec455609eda9b23483e7633579d
NjohPrince <jufredprince@gmail.com>
2025-07-21 07:13:14 +0100

    feat(components): added side images for the product, will be improved later on

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,30 @@ export const metadata: Metadata = {
   title: "EJARALUX | Luxury Skin, Timeless Glow",
   description:
     "Luxury skincare crafted to reveal your most radiant self. Shop glow essentials and elevate your beauty ritual.",
+  openGraph: {
+    title: "EJARALUX | Luxury Skin, Timeless Glow",
+    description:
+      "Luxury skincare crafted to reveal your most radiant self. Shop glow essentials and elevate your beauty ritual.",
+    url: "https://ejaralux.vercel.app",
+    siteName: "EJARALUX",
+    images: [
+      {
+        url: "https://ejaralux.vercel.app/images/products/blur.webp",
+        width: 512,
+        height: 512,
+        alt: "EJARALUX",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "EJARALUX | Luxury Skin, Timeless Glow",
+    description:
+      "Luxury skincare crafted to reveal your most radiant self. Shop glow essentials and elevate your beauty ritual.",
+    images: ["https://ejaralux.vercel.app/images/products/blur.webp"],
+  },
 };
 
 export default function RootLayout({
