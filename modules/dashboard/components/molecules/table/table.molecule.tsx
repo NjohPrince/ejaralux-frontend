e2d28fee05d614e5c862d1f3495b7d625bdc83fe
NjohPrince <jufredprince@gmail.com>
2025-07-26 14:04:40 +0100

    feat(dashboard): worked on the categories table

--- modules/dashboard/components/molecules/table/table.molecule.tsx
+++ modules/dashboard/components/molecules/table/table.molecule.tsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React from "react";
+
+import classes from "./table.module.css";
 
 const TableMolecule = () => {
   return (
-    <div>TableMolecule</div>
-  )
-}
+    <div className={`${classes.container}`}>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>NAME</th>
+            <th>ACTIONS</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>John Doe</td>
+            <td>john.doe@example.com</td>
+            <td>
+              <button
+                className={`${classes["action-button"]} ${classes["edit"]}`}
+              >
+                Edit
+              </button>
+              <button
+                className={`${classes["action-button"]} ${classes["delete"]}`}
+              >
+                Delete
+              </button>
+            </td>
+          </tr>
+          <tr>
+            <td>Jane Smith</td>
+            <td>jane.smith@example.com</td>
+            <td>
+              <button
+                className={`${classes["action-button"]} ${classes["edit"]}`}
+              >
+                Edit
+              </button>
+              <button
+                className={`${classes["action-button"]} ${classes["delete"]}`}
+              >
+                Delete
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
-export default TableMolecule
+export default TableMolecule;
--- modules/dashboard/components/templates/categories/categories.template.tsx
+++ modules/dashboard/components/templates/categories/categories.template.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 import TemplateheaderAtom from "../../atoms/template-header/template-header.atom";
+import TableMolecule from "../../molecules/table/table.molecule";
 
 const DashboardCategoriesTemplate = () => {
   return (
-    <div>
+    <div className={`flex col 32`}>
       <TemplateheaderAtom content="Manage Categories" />
+      <TableMolecule />
     </div>
   );
 };
