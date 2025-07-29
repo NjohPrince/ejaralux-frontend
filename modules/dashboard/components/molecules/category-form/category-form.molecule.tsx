f838a3707930084e745ba67e7cfd5ba42473d6e7
NjohPrince <jufredprince@gmail.com>
2025-07-29 02:48:10 +0100

    fix(eslint): disabled eslint warning for now

--- modules/dashboard/components/molecules/category-form/category-form.molecule.tsx
+++ modules/dashboard/components/molecules/category-form/category-form.molecule.tsx
@@ -32,6 +32,7 @@ const CategoryFormMolecule = ({
     if (mode === "edit" && initialData) {
       setForm({ ...form, name: initialData.name });
     }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [mode, initialData]);
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
